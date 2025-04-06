c9102b103e202fa9bb2f0c0234588f8b572017ad
zooz13 <zo_o13@naver.com>
2025-04-06 23:06:16 +0900

    fix: Chapter14

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,7 +9,7 @@ import {
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { Button } from "@/app/ui/button";
-import { createInvoice, State } from "@/app/lib/actions";
+import { updateInvoice, State } from "@/app/lib/actions";
 import { useActionState } from "react";
 
 export default function EditInvoiceForm({
@@ -24,7 +24,7 @@ export default function EditInvoiceForm({
   const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
 
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
